3c942389a5f37d5890aebacd84ee7ccd30fbac86
Ustych Maksym <ustichmaxim@gmail.com>
2018-12-07 21:32:05 +0200

    try to simplify init data validation, some refactoring

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import Scatter from './scatter-response-js'
 
 window.Scatter = Scatter
+
+Scatter.init('https://httpbin.org/get')
+
+Scatter.get(['count']).then(response => console.log(response))
+Scatter.get(['item']).then(response => console.log(response))
--- src/scatter-response-js.js
+++ src/scatter-response-js.js
@@ -1,22 +1,29 @@
 import axios from 'axios'
 
 class Scatter {
+  // ok
   static init (data) {
     if (this._initialDataIsCorrect(data)) {
-      if (typeof data === 'string') {
-        this._setUrl(data)
-      } else {
-        this._setUrl(data)
-        this._setParams(data)
-      }
+      this._setUrl(data)
     }
     return this
   }
 
-  static get () {
-    return this._handleMultipleRequests(this._sendRequest)
+  static async get (wanted) {
+    Scatter.prototype._wants.push(wanted)
+    let response = await this._handleMultipleRequests(this._sendRequest)
+    response.data.count = 20
+    response.data.item = 'product'
+
+    // this.appendDataToResponse()
+    return new Promise(resolve => {
+      let newData = {}
+      newData[wanted] = response.data[wanted]
+      return resolve(newData)
+    })
   }
 
+  // ok
   static _handleMultipleRequests (fn) {
     if (this._promise != null) {
       return this._promise
@@ -29,11 +36,12 @@ class Scatter {
     return this._promise
   }
 
+  // add additional data to query for axios from multiple get methods
   static _sendRequest () {
     return new Promise((resolve, reject) => {
       setTimeout(async () => {
         try {
-          let response = await axios.get(this._url, { params: this.prototype._params })
+          let response = await axios.get(this.prototype._url)
           return resolve(response)
         } catch (error) {
           return reject(error)
@@ -43,36 +51,45 @@ class Scatter {
   }
 
   // Service methods
+  // ok
   static _setUrl (data) {
     if (typeof data === 'string') {
       this.prototype._url = data
-    } else if (typeof data === 'object') {
-      if (Object.keys(data).includes('url')) {
-        this.prototype._url = data.url
-      }
     } else {
-      throw new Error('Method init() needs at least url to inicialize Scatter')
+      this.prototype._url = data.url
     }
   }
 
-  static _setParams (data) {
-    let params = Object.assign({}, data)
-    delete params.url
-    this.prototype._params = params
-  }
-
-  // Validate methods
+  // Validate methods (extract code below to Validator class)
   static _initialDataIsCorrect (data) {
     const alowedTypes = ['string', 'object']
     let dataType = typeof data
 
-    if (!data || !alowedTypes.includes(dataType) || (dataType === 'object' && Array.isArray(data))) {
+    if (!alowedTypes.includes(dataType)) {
       throw new Error('Method init() needs at least url to inicialize Scatter')
     }
+
+    if (dataType === 'string' && !data.length) {
+      throw new Error('Method init() needs at least url to inicialize Scatter')
+    }
+
+    if (dataType === 'object' && !Object.keys(data).length) {
+      throw new Error('Method init() needs at least url to inicialize Scatter')
+    }
+
+    if (dataType === 'object' && Array.isArray(data)) {
+      throw new Error('Method init() needs at least url to inicialize Scatter')
+    }
+
+    if (dataType === 'object' && !Object.keys(data).includes('url')) {
+      throw new Error('Method init() needs at least url to inicialize Scatter')
+    }
+
     return true
   }
 }
 
 Scatter.prototype._promise = null
+Scatter.prototype._wants = []
 
 export default Scatter
